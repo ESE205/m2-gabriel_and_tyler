27c4c157e646e94716b9f157748d0155b733d545
Gabriel <bangoura@wustl.edu>
2024-01-30 20:59:31 -0600

    final

--- blink_with_write.py
+++ blink_with_write.py
@@ -1,52 +1,29 @@
 import RPi.GPIO as GPIO
 import time
-import argparse
 import sys
 
-# Set up command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--debug", action="store_true", help="print debug info")
-parser.add_argument("-r", "--rate", type=int, default=1, help="blink rate in seconds")
-parser.add_argument("-t", "--time", type=int, default=10, help="program run time in seconds")
-args = parser.parse_args()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-# Set up GPIO
-GPIO.setmode(GPIO.BCM)
 LED_PIN = 16
-SWITCH_PIN = 24
-GPIO.setup(LED_PIN, GPIO.OUT)
-GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+LED_IS_ON = False
+GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 
-# Initialize variables
+DEBUG = '-debug' in sys.argv
+BLINK_RATE = int(sys.argv[sys.argv.index('-blink_rate')+1]) if '-blink_rate' in sys.argv else 1
+RUN_TIME = int(sys.argv[sys.argv.index('-run_time')+1]) if '-run_time' in sys.argv else 60
+
 start_time = time.time()
-iterations = 0
-state = False
 
-# Main loop
-while time.time() - start_time < args.time:
-    # Check switch state
-    if GPIO.input(SWITCH_PIN) == GPIO.HIGH:
-        # Toggle state
-        state = not state
-        GPIO.output(LED_PIN, state)
-        # Debug info
-        if args.debug:
-            print(f"{time.ctime()}\t{iterations}\t{'on' if state else 'off'}")
-        # Write to file
-        with open("output.txt", "a") as f:
-            f.write(f"{time.ctime()}\t{'on' if state else 'off'}\n")
-        # Wait for the next cycle
-        time.sleep(args.rate)
-        iterations += 1
-    else:
-        GPIO.output(LED_PIN, False)
+with open('data.txt', 'w') as data:
+    i = 0
+    while time.time() - start_time < RUN_TIME:
+        GPIO.output(LED_PIN, LED_IS_ON)
+        data.write(f'{time.time():1.0f}\t{LED_IS_ON}\n')
+        if DEBUG:
+            print(f'{time.asctime()}\t{i}\tLED is {"on" if LED_IS_ON else "off"}')
+        LED_IS_ON = not LED_IS_ON
+        time.sleep(BLINK_RATE)
+        i += 1
 
-# Clean up
 GPIO.cleanup()
-
-
-
-
-
-
-
